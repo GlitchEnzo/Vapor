bbf531bd8dd627eaedcf303e5ab00eb9617c2ce8
GlitchEnzo <mccarthp@gmail.com>
2014-06-13 17:30:59 -0700

    Fixed audio to play multiple times
    Fixed a bug where audio could only be played once.
    Added Pause and Stop functionality to audio, but it needs to be
    refactored to be more robust.

--- Audio/AudioSource.ts
+++ Audio/AudioSource.ts
@@ -21,31 +21,58 @@
 
         public loaded: boolean = false;
 
+        // TODO: This needs to be updated to always be tracking the time when the audio is playing
+        //public currentTime: number;
+
+        private startTime: number;
+        private pauseTime: number;
+
         constructor(manager: AudioManager) {
             this.manager = manager;
-            this.source = manager.context.createBufferSource();
-            this.source.connect(manager.context.destination);
+            //this.source = manager.context.createBufferSource();
+            //this.source.connect(manager.context.destination);
         }
 
-        public LoadAudio(url: string, callback: (source: AudioSource) => any) {
-            FileDownloader.DownloadArrayBuffer(url, (request) => {
-                this.manager.context.decodeAudioData(request.response, (buffer) => {
-                    this.source.buffer = buffer;
-                    this.loaded = true;
-                    callback(this);
-                });
-            });
+        //public LoadAudio(url: string, callback: (source: AudioSource) => any) {
+        //    FileDownloader.DownloadArrayBuffer(url, (request) => {
+        //        this.manager.context.decodeAudioData(request.response, (buffer) => {
+        //            this.source.buffer = buffer;
+        //            this.loaded = true;
+        //            callback(this);
+        //        });
+        //    });
+        //}
+
+        public Play(startTime: number = this.pauseTime) {
+            this.source = this.manager.context.createBufferSource();
+            this.source.buffer = this.buffer;
+            this.source.connect(this.manager.context.destination);
+            this.source.start(0, startTime);
+
+            // save the start time 
+            this.startTime = performance.now();
         }
 
-        public Play() {
-            this.source.start(0);
+        public Pause() {
+            this.source.stop(0);
+
+            // NOTE: This method will fail if the song is paused multiple times during playback
+            this.pauseTime = performance.now() - this.startTime;
+        }
+
+        public Stop() {
+            this.source.stop(0);
+
+            this.startTime = 0;
+            this.pauseTime = 0;
         }
 
         public static FromFile(manager: AudioManager, url: string, callback: (source: AudioSource) => any) {
             var source = new AudioSource(manager);
             FileDownloader.DownloadArrayBuffer(url, (request) => {
                 source.manager.context.decodeAudioData(request.response, (buffer) => {
-                    source.source.buffer = buffer;
+                    source.buffer = buffer;
+                    //source.source.buffer = buffer;
                     source.loaded = true;
                     callback(source);
                 });
